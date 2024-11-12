6aa98fc86614900a8ba4c30f3cd161e5f7379e29
Sergej Moor <sergejmoor01@gmail.com>
2024-11-12 22:23:49 +0100

    feat: made navbar responsive

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -4,15 +4,22 @@
 	import Search from 'lucide-svelte/icons/search';
 </script>
 
-<nav class=" flex items-center justify-between border border-b-primary px-4 py-2">
-	<a class="block font-bold leading-[0.9] text-primary" href="/">
-		Your <br />
-		Album <br />
-		Poster <br />
-	</a>
-	<a class="block rounded-full border border-primary p-2 text-primary" href="/search">
-		<Search size={16}></Search>
-	</a>
+<nav class="  border border-b-primary px-4 py-2 md:px-8 lg:px-16">
+	<div class="mx-auto flex items-center justify-between lg:max-w-6xl">
+		<a class="block font-bold leading-[0.9] text-primary lg:hidden" href="/">
+			Your <br />
+			Album <br />
+			Poster <br />
+		</a>
+		<a class=" hidden text-lg font-bold text-primary lg:block" href="/"> Your Album Poster </a>
+		<a
+			class="flex items-center gap-2 rounded-full border border-primary p-2 px-4 text-primary"
+			href="/search"
+		>
+			<span>Search</span>
+			<Search size={14}></Search>
+		</a>
+	</div>
 </nav>
 <div class="flex justify-center">
 	<div class="mx-4 my-2 grow md:mx-8 lg:mx-16 lg:my-16 lg:max-w-6xl">
