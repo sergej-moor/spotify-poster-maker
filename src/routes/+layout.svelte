584402fa9d879137d6140ee3da2c9e00a91c710b
Sergej Moor <sergejmoor01@gmail.com>
2024-11-12 16:51:12 +0100

    feat: navbar

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,9 +1,19 @@
 <script>
 	import '../app.css';
 	let { children } = $props();
+	import Search from 'lucide-svelte/icons/search';
 </script>
 
-<nav class="h-12"></nav>
+<nav class=" flex items-center justify-between border border-b-primary px-4 py-2">
+	<a class="block font-bold leading-[0.9] text-primary" href="/">
+		Your <br />
+		Album <br />
+		Poster <br />
+	</a>
+	<a class="block rounded-full border border-primary p-2 text-primary" href="/search">
+		<Search size={16}></Search>
+	</a>
+</nav>
 <div class="mx-4 my-2">
 	{@render children()}
 </div>
